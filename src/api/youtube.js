382ac608b45ef13591e5c06264cf88b19fc41cbb
Abdullah700 <abdullah.alshaye@ymail.com>
2019-01-03 13:52:53 +0300

    -

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,11 @@
+import axios from 'axios';
 const KEY = 'AIzaSyAMfoC1hcVT16qTpPBEKTnKJQk5MOuVbaY';
+
+export default axios.create({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResults: 5,
+		key: KEY,
+	}
+});
