8060b08bb748f421d06fc35813ad79f66f10962e
abdullah alshaye <abdullah.alshaye@ymail.com>
2019-01-03 21:10:05 +0300

    =

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
-	onTermSubmit = term => {
-		youtube.get('/search', {
-			params:{
-                q: term
-            }
+	state = { videos: [] };
+
+	onTermSubmit = async term => {
+		const res = await youtube.get('/search', {
+			params: {
+				q: term
+			}
 		});
+		this.setState({ videos: res.data.items });
 	};
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
